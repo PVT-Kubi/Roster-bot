74f23dab08471156f211ccddeac0330c2313108f
PVT-Kubi <jast.kuba@gmail.com>
2021-04-18 19:08:18 +0200

    Update roster-bot.py

--- roster-bot.py
+++ roster-bot.py
@@ -636,10 +636,9 @@ async def o(ctx, tabela, pp):
                 print(x[0])
                 member = await findMember(ctx, x[0])
                 prin = ctx.message.guild.get_member(int(x[0]))
-                print(prin)
                 sliced = prin.nick[2:]
                 array = sliced.split("-")
-                pracie +=f"{padStart(f'{sliced[0]}{sliced[1]}{sliced[2]}-{sliced[3]}-{sliced[4]}', 29)} | {padStart(f'{x[1]}', 37)}|\n"
+                pracie +=f"{padStart(f'{array[0]}-{array[1]}-{array[2]}', 29)} | {padStart(f'{x[1]}', 37)}|\n"
             pracie += "```"
             await ctx.send(pracie)
 
