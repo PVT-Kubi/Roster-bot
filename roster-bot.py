7a2630950a8fb706480f34857ed67da9da0cdb51
PVT-Kubi <jast.kuba@gmail.com>
2021-05-16 18:04:20 +0200

    Update roster-bot.py

--- roster-bot.py
+++ roster-bot.py
@@ -11,9 +11,9 @@ import re
 from mysql.connector import pooling
 import MySQLdb
 from MySQLdb.cursors import SSCursor
-from discord.utils import get
-from discord import FFmpegPCMAudio
-from youtube_dl import YoutubeDL
+#from discord.utils import get
+#from discord import FFmpegPCMAudio
+#from youtube_dl import YoutubeDL
 #from discord_slash import SlashCommand
 #from discord_slash.utils.manage_commands import create_option
 
